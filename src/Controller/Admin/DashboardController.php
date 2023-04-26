c0f833460afdc1b6e534203cf1bf9c822ed7f95b
NelsQuick <nelsonpires.pro@gmail.com>
2023-04-26 10:08:28 +0200

    dashboard qui fonctionne

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,7 +16,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Locale;
 
 class DashboardController extends AbstractDashboardController
 {
--- src/Controller/Admin/ReservationCrudController.php
+++ src/Controller/Admin/ReservationCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Reservation;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class ReservationCrudController extends AbstractCrudController
 {
@@ -15,11 +17,10 @@ class ReservationCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
+        yield DateTimeField::new('startDate');
+        yield DateTimeField::new('endDate');
+        yield AssociationField::new('room');
+        yield AssociationField::new('user');
     
+}
 }
